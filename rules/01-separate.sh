8a6a31b78057ed30a9708266901c17601eeca9e7
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-12-23 21:44:55 +0000

    Use short options for mac compat
    Why don't mac's have long options?

--- rules/01-separate.sh
+++ rules/01-separate.sh
@@ -6,4 +6,4 @@ commit_message_file="$1"
 # Pass if message has 1 or fewer lines
 test 1 -ge "$(< $commit_message_file wc -l)" ||
 # Pass if second line is blank
-test -z "$(< $commit_message_file head --lines 2 | tail --lines 1)"
+test -z "$(< $commit_message_file head -n 2 | tail -n 1)"
