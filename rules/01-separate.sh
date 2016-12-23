9f7f7769c83eb7686572422e2c85c15c506472fc
Aaron J. Lang <aaronjameslang@googlemail.com>
2016-12-23 20:30:11 +0000

    Add more tests

--- rules/01-separate.sh
+++ rules/01-separate.sh
@@ -3,5 +3,7 @@ set -eu
 
 commit_message_file="$1"
 
-test 1 = "$(< $commit_message_file wc --lines)" ||
+# Pass if message has 1 or fewer lines
+test 1 -ge "$(< $commit_message_file wc --lines)" ||
+# Pass if second line is blank
 test -z "$(< $commit_message_file head --lines 2 | tail --lines 1)"
--- test/01-separate-test.sh
+++ test/01-separate-test.sh
@@ -4,6 +4,16 @@ set -eu
 rule="./rules/01-separate.sh"
 msg_dir=$PWD/test/01-separate
 
+it_passes_empty() {
+  output="$($rule $msg_dir/empty.msg)"
+  test -z "$output"
+}
+
+it_passes_blank() {
+  output="$($rule $msg_dir/blank.msg)"
+  test -z "$output"
+}
+
 it_passes_subject_only() {
   output="$($rule $msg_dir/subject-only.msg)"
   test -z "$output"
