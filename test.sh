533941460fa0d58380f71714f5f485431dc2854c
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-08 22:16:32 +0000

    Fix bug where checkbashisms' output was silenced

--- test.sh
+++ test.sh
@@ -5,10 +5,18 @@ have () {
   command -v "$1" >/dev/null 2>&1
 }
 
-! have shellcheck && have brew && brew install shellcheck
-have shellcheck && find . -perm -u=x -type f | grep --invert-match '\.git' | xargs shellcheck
+find_scripts () {
+  find . -perm -u=x -type f | grep --invert-match '\.git'
+}
+
+! have shellcheck    && have brew && brew install shellcheck
+  have shellcheck    && find_scripts | xargs shellcheck
 
 ! have checkbashisms && have brew && brew install checkbashisms
-have checkbashisms && find . -perm -u=x -type f | grep --invert-match '\.git' | xargs checkbashisms | true
+if have checkbashisms
+then
+  find_scripts | xargs checkbashisms || true
+fi
+
 have roundup || brew install roundup
 roundup test/*-test.sh
