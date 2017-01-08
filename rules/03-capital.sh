f07ac85137f0ac3f0f872d18d62f97e9ba8bc98d
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-08 22:39:05 +0000

    Quote variable

--- rules/03-capital.sh
+++ rules/03-capital.sh
@@ -4,7 +4,7 @@ set -eu
 commit_message_file="$1"
 
 # Pass if message has no lines
-test 0 -ge "$(< $commit_message_file wc -l)" && exit
+test 0 -ge "$(< "$commit_message_file" wc -l)" && exit
 
 # Fail if first character is lower case
 head -n 1 "$commit_message_file" | grep --invert-match --quiet '^[a-z]'
