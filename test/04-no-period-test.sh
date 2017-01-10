07d85ae5e042877fa8b7db2a5a814f5a7fbf94fe
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-10 23:21:35 +0000

    Quote variables

--- test/04-no-period-test.sh
+++ test/04-no-period-test.sh
@@ -5,21 +5,21 @@ rule="./src/04-no-period.sh"
 msg_dir=$PWD/test/messages
 
 it_passes_empty() {
-  output="$($rule $msg_dir/empty.msg)"
+  output="$($rule "$msg_dir/empty.msg")"
   test -z "$output"
 }
 
 it_passes_blank() {
-  output="$($rule $msg_dir/blank.msg)"
+  output="$($rule "$msg_dir/blank.msg")"
   test -z "$output"
 }
 
 it_passes_perfect() {
-  output="$($rule $msg_dir/perfect.msg)"
+  output="$($rule "$msg_dir/perfect.msg")"
   test -z "$output"
 }
 
 it_fails_period() {
-  output="$(! $rule $msg_dir/period.msg)"
+  output="$(! $rule "$msg_dir/period.msg")"
   test "$output" == 'Do not end the subject line with a period'
 }
