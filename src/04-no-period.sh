5b13b3d569c694b43f2b97c0acb1a86a81de952f
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-10 20:55:33 +0000

    Fix rule 4 for empty messages

--- src/04-no-period.sh
+++ src/04-no-period.sh
@@ -3,6 +3,9 @@ set -eu
 
 commit_message_file="$1"
 
+# Pass if message has no lines
+test 0 -ge "$(< "$commit_message_file" wc -l)" && exit
+
 # Pass if first line does not end with a period
 < "$commit_message_file" head -n 1 | grep --invert-match --quiet '\.$' && exit
 
