00f6f72dafeb8e6c4dafc55e9a2a8e1713fe252f
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-12 18:15:09 +0000

    Add general pass if empty rule

--- src/git-hook-commit-message-cbeams.sh
+++ src/git-hook-commit-message-cbeams.sh
@@ -6,6 +6,8 @@ commit_message_file="$1"
 exit_code=0
 
 validate() {
+  # Pass if message is empty
+  test 0 -eq "$(< "$commit_message_file" wc -l)" && return
   case $1 in
     *) "./src/0$index-"* "$commit_message_file" ;;
   esac
