beae04aee5b175e2547c97856d10a8d1b3a914f2
Aaron J. Lang <aaronjameslang@googlemail.com>
2017-01-30 19:36:09 +0000

    Add merge test cases

--- test.sh
+++ test.sh
@@ -37,6 +37,8 @@ test_message perfect
 test_message long-subject
 test_message long-body
 test_message subject-only
+test_message merge-cli
+test_message merge-github
 test_message body-only 2 'Separate subject from body with a blank line'
 test_message too-long-subject 4 'Limit the subject line to 50 characters'
 test_message lower-case 8 'Capitalize the subject line'
